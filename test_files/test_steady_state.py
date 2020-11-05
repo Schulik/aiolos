9119fe3971d7ca79a8454d83760521c7d48232df
Richard Booth <richardabooth@gmail.com>
2020-11-05 12:04:49 +0000

    extend sw test, reduced cfl

--- test_files/test_steady_state.py
+++ test_files/test_steady_state.py
@@ -38,10 +38,11 @@ if __name__ == "__main__":
     check_steady_state("shock_tube6_H2", [0,0,0])
     check_steady_state("shock_tube7_H2", [4.9e-4, 4.9e-4, 0])
 
-    check_steady_state("soundwave_32_H2", [3.20e-7,]*3)
-    check_steady_state("soundwave_64_H2", [1.26e-7,]*3)
-    check_steady_state("soundwave_128_H2",[3.23e-8,]*3)
-    check_steady_state("soundwave_256_H2",[9.1e-9,]*3)
+    check_steady_state("soundwave_32_H2", [8.72e-8,]*3)
+    check_steady_state("soundwave_64_H2", [2.95e-8,]*3)
+    check_steady_state("soundwave_128_H2",[9.07e-9,]*3)
+    check_steady_state("soundwave_256_H2",[2.51e-9,]*3)
+    check_steady_state("soundwave_512_H2",[6.69e-10,]*3)
 
     check_steady_state("planet_cartesian_H2", [1.5e-12, 6.2e-14, 7.6e-11])
     check_steady_state("planet_spherical_H2", [2.5e-1, 3.5e+2, 1.4e+2])
