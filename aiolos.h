b15d7405c1b6ee4e8ffb9a3701594160886604e0
Schulik <mschulik@ic.ac.uk>
2024-11-13 15:38:41 +0000

    readded tests into local and remote

--- aiolos.h
+++ aiolos.h
@@ -15,7 +15,7 @@
 #include <cmath>
 #include <ctime>
 #include <type_traits>
-#include <gsl/gsl_sf_lambert.h>
+//#include <gsl/gsl_sf_lambert.h>
 #include <Eigen/Core>
 #include <Eigen/Dense>
 #include <Eigen/LU>
